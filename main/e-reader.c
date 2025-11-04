ee673ba6c56709f7e9a1a00acf043dd8418d5998
Jorge Rios <jorgeluisrios47@gmail.com>
2025-11-04 13:25:21 -0600

    cleaned up

--- main/e-reader.c
+++ main/e-reader.c
@@ -49,31 +49,7 @@ void app_main(void)
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
     ESP_ERROR_CHECK(example_connect());
-
-    // Get the ESP32's IP address
-    esp_netif_t *netif = esp_netif_get_handle_from_ifkey("WIFI_STA_DEF");
-    if (netif == NULL)
-    {
-        ESP_LOGE(wifiTAG, "Failed to get esp_netif handle");
-    }
-    else
-    {
-        esp_netif_ip_info_t ip_info;
-        ESP_ERROR_CHECK(esp_netif_get_ip_info(netif, &ip_info));
-        ESP_LOGI(wifiTAG, "ESP32 IP Address: " IPSTR, IP2STR(&ip_info.ip));
-        ESP_LOGI(wifiTAG, "ESP32 Netmask: " IPSTR, IP2STR(&ip_info.netmask));
-        ESP_LOGI(wifiTAG, "ESP32 Gateway: " IPSTR, IP2STR(&ip_info.gw));
-    }
-
-    // // Print out Access Point Information
-    // wifi_ap_record_t ap_info;
-    // ESP_ERROR_CHECK(esp_wifi_sta_get_ap_info(&ap_info));
-    // ESP_LOGI(wifiTAG, "--- Access Point Information ---");
-    // ESP_LOG_BUFFER_HEX("MAC Address", ap_info.bssid, sizeof(ap_info.bssid));
-    // ESP_LOG_BUFFER_CHAR("SSID", ap_info.ssid, sizeof(ap_info.ssid));
-    // ESP_LOGI(wifiTAG, "Primary Channel: %d", ap_info.primary);
-    // ESP_LOGI(wifiTAG, "RSSI: %d", ap_info.rssi);
-
+    
     // mount file system
     esp_vfs_spiffs_conf_t config = {
         .base_path = "/storage",

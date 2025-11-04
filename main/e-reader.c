7928b8c80652025169c3fa27462d9d386c7e5be3
Jorge Rios <jorgeluisrios47@gmail.com>
2025-11-03 18:42:16 -0600

    added a file system(SPIFFS)

--- main/e-reader.c
+++ main/e-reader.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
+#include "esp_spiffs.h"
+#include "esp_log.h"
+
+static const char *TAG = "FileSystem";
 
 void app_main(void)
 {
+    esp_vfs_spiffs_conf_t config = {
+        .base_path = "/storage",
+        .partition_label = NULL,
+        .max_files = 5,
+        .format_if_mount_failed = true};
 
+    esp_err_t result = esp_vfs_spiffs_register(&config);
+
+    size_t total = 0, used = 0;
+    result = esp_spiffs_info(config.partition_label, &total, &used);
+    if (result != ESP_OK)
+    {
+        ESP_LOGE(TAG, "Failed to initiialize SPIFFS(%s)", esp_err_to_name(result));
+    }
+    else
+    {
+        ESP_LOGI(TAG, "Partition size: total: %d, used: %d", total, used);
+    }
+
+    FILE *book = fopen("/storage/frankenstein.txt", "r");
+
+    char line[64];
+    fgets(line, sizeof(line), book);
+    fclose(book);
+    printf("%s\n", line);
 }
